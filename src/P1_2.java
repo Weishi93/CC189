39516da5596bfe4459f61a817b4544553fd3efd1
weishi93 <ws1196@nyu.edu>
2017-03-19 15:25:57 -0400

    Add 1_2 solution 2

--- src/P1_2.java
+++ src/P1_2.java
@@ -27,9 +27,30 @@ public class P1_2 {
         return true;
     }
 
+    static boolean isPermutation_2(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+        int[] count = new int[128];
+        for (int i = 0; i < s1.length(); i++) {
+            count[s1.charAt(i)]++;
+        }
+        for (int i = 0; i < s2.length(); i++) {
+            count[s2.charAt(i)]--;
+            if (count[s2.charAt(i)] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         System.out.println(isPermutation_1("asd", "das"));
         System.out.println(isPermutation_1("asdqwe", "asdzx"));
         System.out.println(isPermutation_1("asd", "asf"));
+
+        System.out.println(isPermutation_2("asd", "das"));
+        System.out.println(isPermutation_2("asdqwe", "asdzx"));
+        System.out.println(isPermutation_2("asd", "asf"));
     }
 }
