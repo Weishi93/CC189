0cd8c24b037beb0f23b3179af6d19f80490fec7b
weishi93 <ws1196@nyu.edu>
2017-03-20 16:53:43 -0400

    Add 2_1 solution 2

--- src/P2_1.java
+++ src/P2_1.java
@@ -25,6 +25,22 @@ public class P2_1 {
         return head;
     }
 
+    static ListNode removeDup_2(ListNode head) {
+        ListNode curt = head;
+        while (curt != null) {
+            ListNode runner = curt;
+            while (runner.next != null) {
+                if (runner.next.val == curt.val) {
+                    runner.next = runner.next.next;
+                } else {
+                    runner = runner.next;
+                }
+            }
+            curt = curt.next;
+        }
+        return head;
+    }
+
     public static void main(String[] args) {
         ListNode n1 = new ListNode(1);
         ListNode n2 = new ListNode(2);
@@ -35,7 +51,7 @@ public class P2_1 {
         n2.next = n3;
         n3.next = n4;
         n4.next = n5;
-        ListNode res = removeDup_1(n1);
+        ListNode res = removeDup_2(n1);
         while (res != null) {
             System.out.println(res.val);
             res = res.next;
