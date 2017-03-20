55dc16345dadb8494b058c205f5a6fbc8cb2f9b1
weishi93 <ws1196@nyu.edu>
2017-03-20 16:46:18 -0400

    Add 2_1

---
+++ src/ListNode.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Wei Shi on 3/20/17.
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    public ListNode(int val) {
+        this.val = val;
+    }
+}
---
+++ src/P2_1.java
@@ -0,0 +1,44 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by Wei Shi on 3/20/17.
+ */
+public class P2_1 {
+    /**
+     * Remove duplicates from an unsorted linked list
+     * @param head
+     * @return
+     */
+    static ListNode removeDup_1(ListNode head) {
+        Set<Integer> set = new HashSet<>();
+        ListNode prev = null, curt = head;
+        while (curt != null) {
+            if (set.contains(curt.val)) {
+                prev.next = curt.next;
+            } else {
+                set.add(curt.val);
+                prev = curt;
+            }
+            curt = curt.next;
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        ListNode n1 = new ListNode(1);
+        ListNode n2 = new ListNode(2);
+        ListNode n3 = new ListNode(3);
+        ListNode n4 = new ListNode(2);
+        ListNode n5 = new ListNode(3);
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
+        ListNode res = removeDup_1(n1);
+        while (res != null) {
+            System.out.println(res.val);
+            res = res.next;
+        }
+    }
+}
